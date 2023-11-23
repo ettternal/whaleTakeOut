224b1561cf7b21df15f45dc707c0152231844f95
qhyang <2194784330@qq.com>
2023-11-23 11:37:40 +0800

    导入前端项目完成

---
+++ src/main/java/com/erha/whale/Config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.erha.whale.Config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射，通过浏览器发送的请求映射到本地的资源
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射！！！");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+
+    }
+}

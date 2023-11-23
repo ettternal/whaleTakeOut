e94849c05cf94104aefe516ec2cb0bf67358ed6f
qhyang <2194784330@qq.com>
2023-11-23 10:52:03 +0800

    使用maven工具创建spting项目初始化模板

---
+++ src/main/java/com/erha/whale/WhaleApplication.java
@@ -0,0 +1,16 @@
+package com.erha.whale;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j  //lombok提供，可以省去实体类中get，set方法的书写通过注解代替，可以直接使用log输出info的日志
+@SpringBootApplication
+public class WhaleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleApplication.class,args);
+        //在类上使用Slf4j就可以在方法中使用log.info在控制台输出日志
+        log.info("项目启动成功！！！");
+    }
+}
